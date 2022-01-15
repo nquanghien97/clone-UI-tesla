4f32a655dbcb194d8fa72f539d4fcec6e296a4e3
Quang Hien <nquanghien97@gmail.com>
2022-01-15 12:55:02 +0700

    edit header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,16 +13,16 @@ function Header() {
     return (
         <div>
             <Container> 
-                <a href="#"><img src="./images/logo.svg"></img></a>
+                <a href="/#"><img src="./images/logo.svg"></img></a>
                 <Anchor>
                     <Menu>
-                    <Link href="#1" title="Model S" />
-                    <Link href="#2" title="Model Y" />
-                    <Link href="#3" title="Model 3" />
-                    <Link href="#4" title="Model X" />
-                    <Link href="#5" title="Solar Roof" />
-                    <Link href="#6" title="Solar Panels" />
-                    <Link href="#7" title="Accessories" />
+                    <Link href="/#1" title="Model S" />
+                    <Link href="/#2" title="Model Y" />
+                    <Link href="/#3" title="Model 3" />
+                    <Link href="/#4" title="Model X" />
+                    <Link href="/#5" title="Solar Roof" />
+                    <Link href="/#6" title="Solar Panels" />
+                    <Link href="/#7" title="Accessories" />
                     </Menu>
                 </Anchor>
                 <RightMenu>
@@ -34,20 +34,20 @@ function Header() {
                     <CloseWrap>
                         <IconClose onClick={() => setCloseSubMenu(false)}/>
                     </CloseWrap>
-                    <li><a href="#">Existing Inventory</a></li>
-                    <li><a href="#">Trade-in</a></li>
-                    <li><a href="#">Used inventory</a></li>
-                    <li><a href="#">Test Drive</a></li>
-                    <li><a href="#">Cybertruck</a></li>
-                    <li><a href="#">Roadster</a></li>
-                    <li><a href="#">Semi</a></li>
-                    <li><a href="#">Charging</a></li>
-                    <li><a href="#">Powerwall</a></li>
-                    <li><a href="#">Commercial Energy</a></li>
-                    <li><a href="#">Utilities</a></li>
-                    <li><a href="#">Find Us</a></li>
-                    <li><a href="#">Support</a></li>
-                    <li><a href="#">Investor Relations</a></li>
+                    <li><a href="/#">Existing Inventory</a></li>
+                    <li><a href="/#">Trade-in</a></li>
+                    <li><a href="/#">Used inventory</a></li>
+                    <li><a href="/#">Test Drive</a></li>
+                    <li><a href="/#">Cybertruck</a></li>
+                    <li><a href="/#">Roadster</a></li>
+                    <li><a href="/#">Semi</a></li>
+                    <li><a href="/#">Charging</a></li>
+                    <li><a href="/#">Powerwall</a></li>
+                    <li><a href="/#">Commercial Energy</a></li>
+                    <li><a href="/#">Utilities</a></li>
+                    <li><a href="/#">Find Us</a></li>
+                    <li><a href="/#">Support</a></li>
+                    <li><a href="/#">Investor Relations</a></li>
                 </SubMenu>
             </Container>
         </div>
